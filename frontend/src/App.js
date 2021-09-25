4e0022e835fef45779578f654ad81690877ea10b
Lin Ni <lin.ni@stonybrook.edu>
2021-09-24 20:01:54 -0400

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./index.css";
+import "./tailwind.css";
 
 function App() {
   var ticker;
@@ -7,16 +9,23 @@ function App() {
   var end_date;
   return (
     <div className="App">
-      <div> Ticker</div>
-      <input id = "ticker">
-      </input>
+      <div class="w-full max-w-xs">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+    </div>
+      {/* <div> Ticker</div>
+      <input id="ticker"></input>
       <div> Start_date</div>
-      <input id = "start_date">
-      </input>
+      <input id="start_date"></input>
       <div> End_date </div>
-      <input id = "end_date" onClick>
-      </input>
-      <img id="Img" src="../buy.jpg" />
+      <input id="end_date" onClick></input>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Button
+      </button> */}
     </div>
   );
 }
