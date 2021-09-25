41862999deceb0a483126e8feac03c15bb2ccd50
Lin Ni <lin.ni@stonybrook.edu>
2021-09-24 20:12:10 -0400

    fixed structure of homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,32 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import "./index.css";
 import "./tailwind.css";
-
+import Homescreen from "./Components/HomeScreen";
 function App() {
   var ticker;
   var start_date;
   var end_date;
-  return (
-    <div className="App">
-      <div class="w-full max-w-xs">
-  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
-      </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
-    </div>
-      {/* <div> Ticker</div>
-      <input id="ticker"></input>
-      <div> Start_date</div>
-      <input id="start_date"></input>
-      <div> End_date </div>
-      <input id="end_date" onClick></input>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-        Button
-      </button> */}
-    </div>
-  );
+  return <Homescreen> </Homescreen>;
 }
 
 export default App;
---
+++ frontend/src/Components/HomeScreen.js
@@ -0,0 +1,6 @@
+import React from "react";
+import InfoScreen from "./InfoScreen";
+const Homescreen = (props) => {
+  return <InfoScreen></InfoScreen>;
+};
+export default Homescreen;
